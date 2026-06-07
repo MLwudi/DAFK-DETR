5f4e37099a9e8ca1f565b315de20489e7844b281
MLwudi <22382asdf22382@gmail.com>
2026-06-07 17:47:54 +0800

    update

--- train.py
+++ train.py
@@ -1,17 +1,14 @@
 import warnings, os
 
 #os.environ["CUDA_VISIBLE_DEVICES"]="0,1"    
-# 多卡训练参考<使用教程.md>下方常见错误和解决方案
+
 warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
-#"/home/lihejiang/RTDETR-main/dataset/data.yaml"
-#"/home/lihejiang/RTDETR-main/hituav/data.yaml"
-#"/home/lihejiang/RTDETR-main/ultralytics/cfg/models/rt-detr/rtdetr-r50-bifpn.yaml"
+
 if __name__ == '__main__':
-    model = RTDETR("/home/lihejiang/rtdetrce/zidingyi.yaml")
-    # model.load('') # loading pretrain weights
-    model.train(data="/home/lihejiang/RTDETR-main/dataset/data.yaml",
+    model = RTDETR("zidingyi.yaml")
+    model.train(data="data.yaml",
                 cache=False,
                 imgsz=640,
                 epochs=250,
@@ -20,5 +17,5 @@ if __name__ == '__main__':
                 device='5', 
                 # resume='', 
                 project='runs/train',
-                name='202657visdroneembsafpnwan',     
+                name='2026',     
                 )
